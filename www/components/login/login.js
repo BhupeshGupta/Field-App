d9dc4dd65e76ec7c7ffb9dc4c8ef36abdb1c8cc8
bhupeshg2 <bhupesh00gupta@gmail.com>
2016-01-12 18:39:55 +0530

    uddate onclick to ng-click

--- www/components/login/login.js
+++ www/components/login/login.js
@@ -1,8 +1,11 @@
 'use strict';
 
 receipt_module.controller('LoginController', ['$scope', '$state', function($scope, $state) {
-  $scope.login = function() {
-//    $state.go('root.home');
-      window.location = '#/home'
+  $scope.login_func = function() {
+    $state.go('root.home');
+    console.log("Hi from Login Function");
   };
+
+console.log("Hi from Login Controller");
+    $scope.msg = 'Working';
 }]);

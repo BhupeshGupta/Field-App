1b90fc6d7750c38a9a888d39e7f9a7bee5389f35
Jaskaran Deol <deol.engg@gmail.com>
2016-02-04 03:29:26 +0530

    gr rewrite done

--- www/components/good_receipt/good_receipt.js
+++ www/components/good_receipt/good_receipt.js
@@ -90,7 +90,10 @@ function goodsReceiptController(
         var signature_blob = FileDataService.dataURItoBlob(signature.dataUrl, 'image/png');
         $cordovaFile.writeFile(cordova.file.dataDirectory, file_name, signature_blob, true)
             .then(function (file) {
-                vm.signature.signatureFile = file;
+                vm.signature.signatureFile = {
+                    dir: cordova.file.dataDirectory,
+                    file: file_name
+                };
             });
 
         // Start Geo-lock
@@ -118,26 +121,39 @@ function goodsReceiptController(
     }
 
     function acceptAndUpload() {
-        var gr_creation_promise = DocumentService.create('Goods Receipt', prepareForErp(vm.user_input), false);
-        gr_creation_promise.then(function () {
-            alert('success');
-        }).catch(function (error) {
-            console.log(error);
-            alert('lol!!');
-        });
-
-        //        gr_creation_promise.then(function () {
-        //            return FileDataService.uploadFileFromDisk(
-        //                vm.signature.cameraFile,
-        //                success.data.requestId, ['Cheque Image'],
-        //                true
-        //            ).catch(function (error) {
-        //                console.log(error);
-        //                return $q.reject("Unable to upload file");
-        //            });
-        //        })
-
-
+        var gr_response = null;
+        return DocumentService.create('Goods Receipt', prepareForErp(vm.user_input), false)
+            .then(function (success) {
+                gr_response = success.data;
+            })
+            .then(function () {
+                return FileDataService.uploadFileFromDisk(
+                    vm.signature.cameraFile,
+                    '', '',
+                    false, {
+                        server: 'erp',
+                        doctype: 'Goods Receipt',
+                        docname: gr_response.data.name,
+                        file_field: 'customer_image',
+                        filename: gr_response.data.name + '_customer.jpg'
+                    }
+                );
+            }).then(function (success) {
+                return FileDataService.uploadFileFromDisk(
+                    vm.signature.signatureFile,
+                    '', '',
+                    false, {
+                        server: 'erp',
+                        doctype: 'Goods Receipt',
+                        docname: gr_response.data.name,
+                        file_field: 'signature',
+                        filename: gr_response.data.name + '_signature.jpg'
+                    });
+            })
+            .catch(function (error) {
+                console.log(error);
+                alert('lol!!');
+            });
     }
 
     function captureImage() {
@@ -176,286 +192,3 @@ function goodsReceiptController(
         return transformed_data;
     }
 }
-
-
-
-receipt_module.controller('good_receipt_controller', function ($scope, $rootScope, $state, $cordovaToast, $cordovaCamera, $cordovaFile, $cordovaGeolocation, get_customer_live, images_link_empty, images_link_filled, get_vehicle_live, create_new_good_receipt, canvas_signature, send_image, $q, DocumentService) {
-    var me = this;
-
-    $scope.new_good_receipt_object = {
-        customer_name: {},
-        item_delievered_name: '',
-        item_delievered_quantity: '',
-        item_received_name: '',
-        item_received_quantity: '',
-        done: false,
-        vehicle_number: '',
-        customer_document_id: '',
-        voucher_id: '',
-        customer_image: {},
-        loc_lat: '',
-        loc_long: ''
-    };
-
-    $scope.new_good_receipt_search_object = {
-        vehicle_search: function (query) {
-            var promise = $q.defer();
-            DocumentService.search('Transportation Vehicle', query, {}).success(function (data) {
-                promise.resolve(data.results);
-            });
-            return promise.promise;
-        },
-        customer_search: function (query) {
-            var promise = $q.defer();
-            DocumentService.search('Customer', query, {
-                enabled: 1,
-                sale_enabled: 1
-            }).success(function (data) {
-                promise.resolve(data.results);
-            });
-            return promise.promise;
-        },
-        item_images_empty: images_link_empty,
-        item_images_filled: images_link_filled,
-        confirm_disable: false,
-        customer_image: '',
-        take_signature_button_disable: false,
-        take_signature_next_disable: false
-    };
-
-    $scope.new_good_receipt = angular.copy($scope.new_good_receipt_object);
-
-    $scope.new_good_receipt_search = angular.copy($scope.new_good_receipt_search_object);
-
-
-    // Log Out Event
-    $scope.$on('log_out_event', function (event, args) {
-        delete $scope.new_good_receipt;
-        $scope.new_good_receipt = angular.copy($scope.new_good_receipt_object);
-        delete $scope.new_good_receipt_search;
-        $scope.new_good_receipt_search = angular.copy($scope.new_good_receipt_search_object);
-    });
-
-
-    // Read Data As URL
-    me.read_data_url = function (path, file) {
-        document.addEventListener('deviceready', function () {
-            $cordovaFile.readAsDataURL(path, file).then(function (success) {
-                $scope.new_good_receipt_search.customer_image = success;
-            }, function (error) {
-                console.log(error);
-            });
-        });
-    };
-
-
-    // Create Good Receipt
-    me.create_good_receipt = function () {
-        $scope.new_good_receipt_search.confirm_disable = true;
-        now_date = moment().format("YYYY-MM-DD");
-        now_time = moment().format("HH:mm:ss");
-        voucher_id = $scope.new_good_receipt.voucher_id.toString();
-        data = {
-            docstatus: 1,
-            customer: $scope.new_good_receipt.customer_name.value,
-            item_delivered: $scope.new_good_receipt.item_delievered_name,
-            delivered_quantity: $scope.new_good_receipt.item_delievered_quantity,
-            item_received: $scope.new_good_receipt.item_received_name,
-            received_quantity: $scope.new_good_receipt.item_received_quantity,
-            transaction_date: now_date,
-            posting_date: now_date,
-            posting_time: now_time,
-            vehicle: $scope.new_good_receipt.vehicle_number.value,
-            goods_receipt_number: voucher_id,
-            customer_document_id: $scope.new_good_receipt.customer_document_id,
-            location_latitude: $scope.new_good_receipt.loc_lat,
-            location_longitude: $scope.new_good_receipt.loc_long
-        };
-        create_new_good_receipt.create_feed(data)
-            .success(function (data) {
-                $scope.new_good_receipt_search.confirm_disable = false;
-                send_image.send(voucher_id, $scope.new_good_receipt_search.customer_image, 'gr_' + voucher_id + '_customer_image.jpg', 'Goods Receipt', 'customer_image').success(function (data) {
-                    $scope.new_good_receipt_search.customer_image = "";
-                    $scope.new_good_receipt.customer_image = {};
-                });
-                send_image.send(voucher_id, canvas_signature.signature, 'gr_' + voucher_id + '_signature.jpg', 'Goods Receipt', 'signature').success(function (data) {});
-                delete $scope.new_good_receipt;
-                $scope.new_good_receipt = angular.copy($scope.new_good_receipt_object);
-                delete $scope.new_good_receipt_search;
-                $scope.new_good_receipt_search = angular.copy($scope.new_good_receipt_search_object);
-                $state.transitionTo('root.select_receipt');
-            })
-            .error(function (data) {
-                $scope.new_good_receipt_search.confirm_disable = false;
-                if (data._server_messages)
-                    message = JSON.parse(data._server_messages);
-                else
-                    message = "Server Error";
-                $cordovaToast.show(message, 'short', 'bottom');
-            });
-    };
-
-
-    // Take Image from camera
-    me.take_img = function () {
-        document.addEventListener("deviceready", function () {
-
-            var options = {
-                quality: 50,
-                destinationType: Camera.DestinationType.FILE_URI,
-                sourceType: Camera.PictureSourceType.CAMERA,
-                allowEdit: false,
-                encodingType: Camera.EncodingType.PNG,
-                cameraDirection: Camera.Direction.FRONT
-            };
-
-            $cordovaCamera.getPicture(options).then(function (imageURI) {
-                var image_name = imageURI.substring(imageURI.lastIndexOf('/') + 1);
-                $scope.new_good_receipt.customer_image = {
-                    path: cordova.file.dataDirectory,
-                    name: image_name
-                };
-                me.file_move(image_name);
-                $cordovaCamera.cleanup().then(); // only for FILE_URI
-            }, function (err) {
-                $scope.new_good_receipt_search.take_signature_next_disable = false;
-                $cordovaToast.show("Camera Not Working", 'short', 'bottom');
-                console.log(err);
-            });
-        }, false);
-    };
-
-
-    // Move File from one location to other
-    me.file_move = function (file_name) {
-        document.addEventListener('deviceready', function () {
-            $cordovaFile.moveFile(cordova.file.externalCacheDirectory, file_name, cordova.file.dataDirectory)
-                .then(function (success) {
-                    me.read_data_url(cordova.file.dataDirectory, file_name);
-                    $scope.new_good_receipt_search.take_signature_next_disable = false;
-                    $state.transitionTo('root.good_receipt.take_picture_location');
-                    me.geo_location();
-                }, function (error) {
-                    $scope.new_good_receipt_search.take_signature_next_disable = false;
-                    $cordovaToast.show("File Not Moved", 'short', 'bottom');
-                    console.log(error);
-                });
-        });
-    };
-
-
-    // Geo Location
-    me.geo_location = function () {
-        document.addEventListener('deviceready', function () {
-            var posOptions = {
-                timeout: 10000,
-                enableHighAccuracy: false
-            };
-            $cordovaGeolocation
-                .getCurrentPosition(posOptions)
-                .then(function (position) {
-                    $scope.new_good_receipt.loc_lat = position.coords.latitude;
-                    $scope.new_good_receipt.loc_long = position.coords.longitude;
-                }, function (err) {
-                    $cordovaToast.show("Location not taken", 'short', 'bottom');
-                    console.log(err);
-                });
-        });
-    };
-
-    $scope.customer_name_next = function () {
-        $state.transitionTo('root.good_receipt.item_delievered_name');
-    };
-
-    $scope.item_delievered_name_empty_next = function (index) {
-        $scope.new_good_receipt.item_delievered_name = $scope.new_good_receipt_search.item_images_empty[index].id;
-        $state.transitionTo('root.good_receipt.item_delievered_quantity');
-    };
-
-    $scope.item_delievered_name_filled_next = function (index) {
-        $scope.new_good_receipt.item_delievered_name = $scope.new_good_receipt_search.item_images_filled[index].id;
-        $state.transitionTo('root.good_receipt.item_delievered_quantity');
-    };
-
-    $scope.item_delievered_name_next = function () {
-        $state.transitionTo('root.good_receipt.item_received_name');
-    };
-
-    $scope.item_delievered_quantity_next = function () {
-        $state.transitionTo('root.good_receipt.item_received_name');
-    };
-
-    $scope.item_received_name_empty_next = function (index) {
-        $scope.new_good_receipt.item_received_name = $scope.new_good_receipt_search.item_images_empty[index].id;
-        $state.transitionTo('root.good_receipt.item_received_quantity');
-    };
-
-    $scope.item_received_name_filled_next = function (index) {
-        $scope.new_good_receipt.item_received_name = $scope.new_good_receipt_search.item_images_filled[index].id;
-        $state.transitionTo('root.good_receipt.item_received_quantity');
-    };
-
-    $scope.item_received_name_next = function () {
-        $state.transitionTo('root.good_receipt.acknowledgement');
-    };
-
-    $scope.item_received_quantity_next = function () {
-        $state.transitionTo('root.good_receipt.acknowledgement');
-    };
-
-    $scope.take_signature_button = function () {
-        $scope.new_good_receipt_search.take_signature_button_disable = true;
-        html2canvas(document.getElementById('get_acknowledgement_information'), {
-            onrendered: function (canvas) {
-                canvas_signature.back_image = canvas.toDataURL();
-                $scope.new_good_receipt_search.take_signature_button_disable = false;
-                $rootScope.$emit('signature_canvas_clear', {});
-                $state.transitionTo('root.good_receipt.take_signature');
-            }
-        });
-    };
-
-    $scope.take_signature_next = function () {
-        canvas_signature.signature = canvas_signature.signature_pad.toDataURL('image/jpeg');
-        $scope.new_good_receipt_search.take_signature_next_disable = true;
-        me.take_img();
-    };
-
-    $scope.take_new_image = function () {
-        $cordovaFile.removeFile(cordova.file.dataDirectory, $scope.new_good_receipt.customer_image.name);
-        me.take_img();
-    };
-
-    $scope.confirm_good_receipt = function () {
-        me.create_good_receipt();
-    };
-});
-
-
-
-// Signature Pad Controller
-
-receipt_module.controller('take_signature_controller', function ($scope, $rootScope, canvas_signature) {
-    document.getElementById("signature_canvas_div").innerHTML = "<canvas id='signatureCanvas' style='border: 1px solid black;'></canvas>";
-    var canvas = document.getElementById('signatureCanvas'),
-        ctx = canvas.getContext("2d");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight - 170;
-    var background = new Image();
-    background.src = canvas_signature.back_image;
-
-    background.onload = function () {
-        ctx.drawImage(background, 0, 0, background.width, background.height, 0, 0, canvas.width, canvas.height);
-    };
-
-    canvas_signature.signature_pad = new SignaturePad(canvas);
-
-    $scope.clear_canvas = function () {
-        canvas_signature.signature_pad.clear();
-        background.src = canvas_signature.back_image;
-    };
-
-    $rootScope.$on('signature_canvas_clear', function () {
-        $scope.clear_canvas();
-    });
-});
--- www/js/file.service.js
+++ www/js/file.service.js
@@ -35,7 +35,7 @@ function fileDataService($http, SettingsFactory, $cordovaFile) {
         writeToFile: writeToFile
     };
 
-    function uploadFileFromDisk(file, requestId, tags, datauriformat) {
+    function uploadFileFromDisk(file, requestId, tags, datauriformat, opts) {
         return $cordovaFile
             .readAsDataURL(file.dir, file.file).then(function (file_data) {
                 var file_type;
@@ -47,31 +47,17 @@ function fileDataService($http, SettingsFactory, $cordovaFile) {
                     data: file_data,
                     name: 'file',
                     type: file_type === undefined ? '' : file_type
-                }, requestId, tags);
+                }, requestId, tags, opts);
             });
     }
 
-    function uploadFile(file, requestId, tags) {
-        // Preserve Order, Node Skipper Plugin expects add feilds before file data
-        var fd = new FormData();
-        fd.append('requestId', requestId);
-        fd.append('tags', JSON.stringify(tags));
-        fd.append('file', new Blob([file.data], {
-            type: file.type
-        }), file.name);
+    function uploadFile(file, requestId, tags, opts) {
+        var opts = opts || {};
 
-        var uploadUrl = SettingsFactory.getReviewServerBaseUrl() +
-            '/files/uploadmultipart/';
-
-        return $http.post(uploadUrl, fd, {
-            transformRequest: angular.identity,
-            headers: {
-                'Content-Type': undefined
-            }
-        }).then(function (res) {
-            console.log(JSON.stringify(res.data));
-        });
-
+        if (!opts.server || opts.server === 'review')
+            return uploadToReviewServer(file, requestId, tags);
+        else if (opts.server && opts.server === 'erp')
+            return uploadToErp(file, requestId, tags, opts);
     }
 
     function dataURItoBlob(dataURI, imgType) {
@@ -95,36 +81,7 @@ function fileDataService($http, SettingsFactory, $cordovaFile) {
         $cordovaFile.writeFile(file.dir, file.name, data, true);
     }
 
-}
-
-angular.module('starter')
-    .factory('ErpFileDataService', erpFileDataService);
-
-function erpFileDataService($http, SettingsFactory, $cordovaFile) {
-    return {
-        uploadFileFromDisk: uploadFileFromDisk,
-        uploadFile: uploadFile,
-        dataURItoBlob: dataURItoBlob,
-        writeToFile: writeToFile
-    };
-
-    function uploadFileFromDisk(file, requestId, tags, datauriformat) {
-        return $cordovaFile
-            .readAsDataURL(file.dir, file.file).then(function (file_data) {
-                var file_type;
-                if (datauriformat) {
-                    file_type = file_data.split(',')[0].split(';')[0].split(':')[1];
-                    file_data = dataURItoBlob(file_data);
-                }
-                return uploadFile({
-                    data: file_data,
-                    name: 'file',
-                    type: file_type === undefined ? '' : file_type
-                }, requestId, tags);
-            });
-    }
-
-    function uploadFile(file, requestId, tags) {
+    function uploadToReviewServer(file, requestId, tags) {
         // Preserve Order, Node Skipper Plugin expects add feilds before file data
         var fd = new FormData();
         fd.append('requestId', requestId);
@@ -144,28 +101,39 @@ function erpFileDataService($http, SettingsFactory, $cordovaFile) {
         }).then(function (res) {
             console.log(JSON.stringify(res.data));
         });
-
     }
 
-    function dataURItoBlob(dataURI, imgType) {
-        var isSemicolonExist = (dataURI.indexOf(',') >= 0) ? true : false;
-        var binary = [];
-        if (isSemicolonExist)
-            binary = atob(dataURI.split(',')[1]);
-        else
-            binary = atob(dataURI);
-        var array = [];
-        for (var i = 0; i < binary.length; i++) {
-            array.push(binary.charCodeAt(i));
-        }
-        var blob = new Blob([new Uint8Array(array)], {
-            type: imgType
+    function uploadToErp(file, requestId, tags, opts) {
+        //doctype: doctype,
+        //docname: voucher_id,
+        //file_field: type,
+        //filename: filename
+
+        var fd = new FormData();
+        var params = $.extend(opts, {
+            from_form: '1',
+            client: 'app',
+            cmd: 'uploadfile',
+            _type: 'POST'
+        }, {
+            filedata: file.data
         });
-        return blob;
-    }
 
-    function writeToFile(file, data) {
-        $cordovaFile.writeFile(file.dir, file.name, data, true);
+        for (var key in params) {
+            fd.append(key, params[key]);
+        }
+
+        return $http.post(SettingsFactory.getERPServerBaseUrl(), fd, {
+            transformRequest: angular.identity,
+            headers: {
+                'Content-Type': undefined
+            }
+        }).then(function (res) {
+            console.log(JSON.stringify(res.data));
+        });
+
+        //        return $http.post(SettingsFactory.getERPServerBaseUrl(), $.param(request));
+
     }
 
 }

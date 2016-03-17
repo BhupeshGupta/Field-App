00950876ef0b6e2fa6e192c474131ac6f1d3e4c3
Jaskaran Deol <deol.engg@gmail.com>
2016-03-17 17:34:52 +0530

    Disable non working modules Remove `Val Controller` as it was not required

--- www/js/app.js
+++ www/js/app.js
@@ -56,154 +56,150 @@ var receipt_module = angular.module('starter', [
 //})
 
 .config(function ($stateProvider, $urlRouterProvider, $compileProvider) {
-        $urlRouterProvider.otherwise('/home');
-        $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|file|blob|cdvfile):|data:image\//);
+    $urlRouterProvider.otherwise('/home');
+    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|file|blob|cdvfile):|data:image\//);
 
-        $stateProvider.state('root', {
-            templateUrl: 'components/main/main.html',
-            controller: 'MainController',
-            abstract: true
-        })
-
-        .state('root.developer', {
-            url: '/developer',
-            templateUrl: 'components/developer/developer.html',
-            controller: 'DeveloperController'
-        })
-
-        .state('root.login', {
-            url: '/',
-            templateUrl: 'components/login/login.html',
-            controller: 'LoginController',
-            resolve: {
-                settings: function (SettingsFactory, $state, $timeout) {
-                    var settings = SettingsFactory.get();
-                    if (typeof settings.sid !== 'undefined') {
-                        $timeout(function () {
-                            $state.go('root.home');
-                        }, 0);
-                    }
-                }
-            }
-        })
-
-        .state('root.db', {
-            url: '/db',
-            templateUrl: 'components/db/db.html',
-            controller: 'DbController',
-            controllerAs: 'dbc'
-        })
-
-
-        .state('root.home', {
-            url: '/home',
-            templateUrl: 'components/home/home.html',
-            controller: 'HomeController',
-            resolve: {
-                user: function (UserService, $q) {
-                    var defer = $q.defer();
-                    UserService.loadUser().then(function () {
-                        defer.resolve();
-                    });
-                    return defer.promise;
-                }
-            }
-        })
-
-        .state('root.invoice', {
-                url: '/invoice',
-                templateUrl: 'components/invoice/invoice.html',
-                controller: 'InvoiceFlowController'
-            })
-            .state('root.invoice.step1', {
-                url: '/step1',
-                views: {
-                    'invoice_view': {
-                        templateUrl: 'components/invoice/forms/step_1.html'
-                    }
-                }
-            })
-            .state('root.invoice.step1-1', {
-                url: '/step1-1',
-                views: {
-                    'invoice_view': {
-                        templateUrl: 'components/invoice/forms/step_1-1.html'
-                    }
-                }
-            })
-            .state('root.invoice.step2', {
-                url: '/step2',
-                views: {
-                    'invoice_view': {
-                        templateUrl: 'components/invoice/forms/step_2.html'
-                    }
-                }
-            })
-            .state('root.invoice.step3', {
-                url: '/step3',
-                views: {
-                    'invoice_view': {
-                        templateUrl: 'components/invoice/forms/step_3.html'
-                    }
-                }
-            })
-            .state('root.invoice.step4', {
-                url: '/step4',
-                views: {
-                    'invoice_view': {
-                        templateUrl: 'components/invoice/forms/step_4.html'
-                    }
-                }
-            })
-
-        .state('root.cheque', {
-                url: '/cheque',
-                templateUrl: 'components/cheque/cheque.html',
-                controller: 'chequeFlowController',
-                controllerAs: 'cc'
-            })
-            .state('root.cheque.details', {
-                url: '/cheque/cheque_details',
-                views: {
-                    'cheque_view': {
-                        templateUrl: 'components/cheque/forms/cheque_detail.html'
-                    }
-                }
-            })
-            .state('root.cheque.review', {
-                url: '/cheque/cheque_review',
-                views: {
-                    'cheque_view': {
-                        templateUrl: 'components/cheque/forms/cheque_review.html'
-                    }
-                }
-            })
-
-        .state('root.cash', {
-                url: '/cheque',
-                templateUrl: 'components/cash/cash.html',
-                controller: 'cashFlowController',
-                controllerAs: 'cc'
-            })
-            .state('root.cash.details', {
-                url: '/cash/cash/cash_details',
-                views: {
-                    'cash_view': {
-                        templateUrl: 'components/cash/forms/cash_details.html'
-                    }
-                }
-            })
-            .state('root.cash.review', {
-                url: '/cash/cash_review',
-                views: {
-                    'cash_view': {
-                        templateUrl: 'components/cash/forms/cash_signature.html'
-                    }
-                }
-            });
+    $stateProvider.state('root', {
+        templateUrl: 'components/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'mc',
+        abstract: true
     })
-    .controller("ValController", function ($scope) {
-        $scope.submit = function (username) {
-            alert("Thanks " + username);
-        };
-    });
+
+    .state('root.developer', {
+        url: '/developer',
+        templateUrl: 'components/developer/developer.html',
+        controller: 'DeveloperController'
+    })
+
+    .state('root.login', {
+        url: '/',
+        templateUrl: 'components/login/login.html',
+        controller: 'LoginController',
+        resolve: {
+            settings: function (SettingsFactory, $state, $timeout) {
+                var settings = SettingsFactory.get();
+                if (typeof settings.sid !== 'undefined') {
+                    $timeout(function () {
+                        $state.go('root.home');
+                    }, 0);
+                }
+            }
+        }
+    })
+
+    .state('root.db', {
+        url: '/db',
+        templateUrl: 'components/db/db.html',
+        controller: 'DbController',
+        controllerAs: 'dbc'
+    })
+
+
+    .state('root.home', {
+        url: '/home',
+        templateUrl: 'components/home/home.html',
+        controller: 'HomeController',
+        resolve: {
+            user: function (UserService, $q) {
+                var defer = $q.defer();
+                UserService.loadUser().then(function () {
+                    defer.resolve();
+                });
+                return defer.promise;
+            }
+        }
+    })
+
+    .state('root.invoice', {
+            url: '/invoice',
+            templateUrl: 'components/invoice/invoice.html',
+            controller: 'InvoiceFlowController'
+        })
+        .state('root.invoice.step1', {
+            url: '/step1',
+            views: {
+                'invoice_view': {
+                    templateUrl: 'components/invoice/forms/step_1.html'
+                }
+            }
+        })
+        .state('root.invoice.step1-1', {
+            url: '/step1-1',
+            views: {
+                'invoice_view': {
+                    templateUrl: 'components/invoice/forms/step_1-1.html'
+                }
+            }
+        })
+        .state('root.invoice.step2', {
+            url: '/step2',
+            views: {
+                'invoice_view': {
+                    templateUrl: 'components/invoice/forms/step_2.html'
+                }
+            }
+        })
+        .state('root.invoice.step3', {
+            url: '/step3',
+            views: {
+                'invoice_view': {
+                    templateUrl: 'components/invoice/forms/step_3.html'
+                }
+            }
+        })
+        .state('root.invoice.step4', {
+            url: '/step4',
+            views: {
+                'invoice_view': {
+                    templateUrl: 'components/invoice/forms/step_4.html'
+                }
+            }
+        })
+
+    .state('root.cheque', {
+            url: '/cheque',
+            templateUrl: 'components/cheque/cheque.html',
+            controller: 'chequeFlowController',
+            controllerAs: 'cc'
+        })
+        .state('root.cheque.details', {
+            url: '/cheque/cheque_details',
+            views: {
+                'cheque_view': {
+                    templateUrl: 'components/cheque/forms/cheque_detail.html'
+                }
+            }
+        })
+        .state('root.cheque.review', {
+            url: '/cheque/cheque_review',
+            views: {
+                'cheque_view': {
+                    templateUrl: 'components/cheque/forms/cheque_review.html'
+                }
+            }
+        })
+
+    .state('root.cash', {
+            url: '/cheque',
+            templateUrl: 'components/cash/cash.html',
+            controller: 'cashFlowController',
+            controllerAs: 'cc'
+        })
+        .state('root.cash.details', {
+            url: '/cash/cash/cash_details',
+            views: {
+                'cash_view': {
+                    templateUrl: 'components/cash/forms/cash_details.html'
+                }
+            }
+        })
+        .state('root.cash.review', {
+            url: '/cash/cash_review',
+            views: {
+                'cash_view': {
+                    templateUrl: 'components/cash/forms/cash_signature.html'
+                }
+            }
+        });
+});
